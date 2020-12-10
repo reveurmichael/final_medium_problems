27e2e4845c8a3f2b2f7c3f0dc40b0977a0d5900a
Lunde CHEN <lunde.chen@yahoo.fr>
2020-12-10 16:40:09 +0800

    m

--- src/main/java/final_medium/Monster.java
+++ src/main/java/final_medium/Monster.java
@@ -15,6 +15,7 @@ public class Monster implements Comparable<Monster> {
 
     @Override
     public int compareTo(Monster other) {
+        // TODO: modify your code here for the function sortArrUsingComparableInterface()
         return -1;
     }
 }
@@ -23,25 +24,25 @@ class Duck extends Monster {
     public Duck(){
         this.name = "Duck";
         this.strength = 8  + new Random().nextInt(10);
-        this.agility = 18 + new Random().nextInt(10);
-        this.intelligence = 1 + new Random().nextInt(10);
+        this.agility = 8 + new Random().nextInt(10);
+        this.intelligence = 8 + new Random().nextInt(10);
     }
 }
 
 class Dragon extends Monster {
     public Dragon(){
         this.name = "Dragon";
-        this.strength = 9  + new Random().nextInt(10);
-        this.agility = 39 + new Random().nextInt(10);
-        this.intelligence = 9 + new Random().nextInt(10);
+        this.strength = 29  + new Random().nextInt(10);
+        this.agility = 29 + new Random().nextInt(10);
+        this.intelligence = 29 + new Random().nextInt(10);
     }
 }
 
 class Tortoise extends Monster {
     public Tortoise(){
         this.name = "Tortoise";
-        this.strength = 7  + new Random().nextInt(10);
+        this.strength = 1  + new Random().nextInt(10);
         this.agility = 1 + new Random().nextInt(10);
-        this.intelligence = 47 + new Random().nextInt(10);
+        this.intelligence = 1 + new Random().nextInt(10);
     }
 }
